ce99488605f5a275e90393e24734e04ca0e764f2
Contel87 <luigimdg@hotmail.it>
2017-01-12 20:14:13 +0100

    Create IOutCurrent.ino

---
+++ examples/IOutCurrent/IOutCurrent.ino
@@ -0,0 +1,15 @@
+#include <P9025A.h>
+
+P9025A pwr;
+
+void setup() {
+  Serial.begin(9600);
+  pwr.begin();
+}
+
+void loop() {
+  unsigned int IOut;
+  IOut = pwr.getIoutCurrent();
+  Serial.println(IOut*(1.8/4096));
+  delay(50);
+}
