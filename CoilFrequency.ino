4e48a3d4b5a3be3e06f33a73fe1dd4f75204a7ae
Contel87 <luigimdg@hotmail.it>
2017-01-12 20:09:28 +0100

    Add files via upload

---
+++ CoilFrequency.ino
@@ -0,0 +1,15 @@
+#include <P9025A.h>
+
+P9025A pwr;
+
+void setup() {
+  Serial.begin(9600);
+  pwr.begin();
+}
+
+void loop() {
+  unsigned int CoilFreq;
+  CoilFreq = pwr.getCoilFrequency();
+  Serial.println(65536/CoilFreq);
+  delay(50);
+}
