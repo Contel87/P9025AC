87e7f03fed40d0a8de02a9c9a64ccc128502a0c3
Contel87 <luigimdg@hotmail.it>
2017-01-14 11:07:48 +0100

    Update P9025AndPCD8544.ino

--- examples/P9025AndPCD8544/P9025AndPCD8544.ino
+++ examples/P9025AndPCD8544/P9025AndPCD8544.ino
@@ -14,22 +14,25 @@ void setup() {
   lcd.begin(84, 48);
   pwr.begin();
 
-}
-
-
-void loop() {
   // Write a piece of text on the first line...
   lcd.setCursor(0, 0);
   lcd.print("#WirelessPower Contest 2017");
 
-  
+}
+
+
+void loop() {
+
+  //Set Cursor to line 3 and print Vrect value
   lcd.setCursor(0, 3);
   Vrect = pwr.getVrectVoltage();
   lcd.print("Vrect: ");lcd.print(Vrect*5*(1.8/4096));lcd.print(" V ");
 
+  //Print Curret used from load
   IOut = pwr.getIoutCurrent();
   lcd.print("IOut: ");lcd.print(IOut*(1.8/4096));lcd.print(" A  ");
 
+  //Print Coil Resonance frequency 
   CoilFreq = pwr.getCoilFrequency();
   lcd.print("Freq: ");lcd.print(65536/CoilFreq);lcd.print(" Hz  ");
 
