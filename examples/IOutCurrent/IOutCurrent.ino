e158df577562abc174b19fbed59020d99680dbb7
Contel87 <luigimdg@hotmail.it>
2017-01-22 10:53:00 +0100

    Update IOutCurrent.ino

--- examples/IOutCurrent/IOutCurrent.ino
+++ examples/IOutCurrent/IOutCurrent.ino
@@ -1,3 +1,16 @@
+/**************************************************************************/
+/*!
+    @file     P9025A.CPP
+    @author   Conte Luigi
+    @license  GPL 3.0 (see license.txt)
+    This program is wrote for the #WirelessPowerContest.
+    I used I2C line of P9025A to communicate with Arduino UNO.
+    @section  HISTORY
+    v1.0  - First release (January 2017)
+    V1.1 - Coil Frequency unit corrected: in V1.0 Coil frequency value was in Hz... True value is Khz (As QI 1.1 Specifications).
+*/
+/**************************************************************************/
+
 #include <P9025A.h>
 
 P9025A pwr;
